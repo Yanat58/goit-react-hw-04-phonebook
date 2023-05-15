264c8bc43b59acabfc7e72341bbdeef800c19b62
Tetiana Karlova <tanyalyt80@gmail.com>
2023-05-16 02:09:11 +0300

    fix, add prop-type

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,15 +8,15 @@ import { Filter } from 'components/Filter/Filter';
 import { Modal } from 'components/Modal/Modal';
 import css from 'components/App.module.css';
 
-export const App = () => {
-  const initialContact = [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ];
-  const contact = window.localStorage.getItem('contacts');
+const initialContact = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+const contact = window.localStorage.getItem('contacts');
 
+export const App = () => {
   const [filter, setFilter] = useState('');
   const [showModal, setShowModal] = useState(false);
   const [contacts, setContacts] = useState(() => {
