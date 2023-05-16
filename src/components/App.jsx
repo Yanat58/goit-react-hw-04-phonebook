bf81b5ab0ba4e105b1e90352ecc8e8072e84625a
Tetiana Karlova <tanyalyt80@gmail.com>
2023-05-16 20:05:42 +0300

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,13 +14,14 @@ const initialContact = [
   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
 ];
-const contact = window.localStorage.getItem('contacts');
 
 export const App = () => {
   const [filter, setFilter] = useState('');
   const [showModal, setShowModal] = useState(false);
   const [contacts, setContacts] = useState(() => {
-    return JSON.parse(contact) ?? initialContact;
+    return (
+      JSON.parse(window.localStorage.getItem('contacts')) ?? initialContact
+    );
   });
 
   useEffect(() => {
